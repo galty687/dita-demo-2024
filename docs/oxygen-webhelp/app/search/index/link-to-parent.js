bbd2b70f75c6121fd7573ba37fe7e5f65ba576fc
Zhijun Gao <galty687@gmail.com>
2024-11-12 16:42:10 +0800

    fix the broken urls

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {7:2,4:1,2:-1,6:1,1:-1};
+var linkToParent = {6:2,5:2,4:2,2:-1,8:3,7:2,3:-1,0:3};
